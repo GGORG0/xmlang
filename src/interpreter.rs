d7e57226f80a3abd3d472e062f67b3ecc64d8700
GGORG <GGORG0@protonmail.com>
2025-07-15 18:39:43 +0200

    added throw and unwrap

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -105,12 +105,59 @@ pub fn interpret(element: &Element, depth: u32) -> Result<Value> {
             Value::Null
         }
 
+        name @ ("unwrap" | "expect") => {
+            ensure!(
+                element.children.len() == 1,
+                "Expected exactly one child in <{name}> element"
+            );
+
+            let child = &element.children[0];
+            let value = interpret(child, depth + 1)?;
+
+            if value.is_null() {
+                let msg = element
+                    .attributes
+                    .get("message")
+                    .cloned()
+                    .unwrap_or_else(|| "Unwrapped value is null".to_string());
+
+                bail!(msg);
+            }
+
+            value
+        }
+
+        "throw" => {
+            if let Some(msg) = element.attributes.get("message").cloned() {
+                bail!(msg);
+            } else {
+                let text = element.children.iter().try_fold(
+                    element.attributes.get("_text").cloned().unwrap_or_default(),
+                    |value, child| {
+                        let child_value = interpret(child, depth + 1)?;
+
+                        Ok::<_, Report>(if child_value.is_null() {
+                            value
+                        } else {
+                            format!("{value}{child_value}")
+                        })
+                    },
+                )?;
+
+                if text.is_empty() {
+                    bail!("An error occurred, but no message was provided.");
+                } else {
+                    bail!(text);
+                }
+            }
+        }
+
         "add" | "sum" => element
             .children
             .iter()
             .map(|child| interpret(child, depth + 1))
             .sum::<Result<Value>>()?,
-        
+
         "mul" | "multiply" | "product" => element
             .children
             .iter()
